b5e7eebc0224386de14859619d2217bf21417354
tigrisanimus <tigrisanimus@gmail.com>
2025-12-22 15:53:39 -0500

    Add Ankr BNB yield strategy and tests

---
+++ test/mocks/MockAnkrPool.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {MockERC20} from "./MockERC20.sol";
+
+contract MockAnkrPool {
+    MockERC20 public immutable ankrBNB;
+    uint256 public exchangeRatio;
+
+    constructor(address ankrBNB_, uint256 exchangeRatio_) {
+        ankrBNB = MockERC20(ankrBNB_);
+        exchangeRatio = exchangeRatio_;
+    }
+
+    function setExchangeRatio(uint256 newRatio) external {
+        exchangeRatio = newRatio;
+    }
+
+    function stakeCerts() external payable {
+        require(msg.value > 0, "ZERO_STAKE");
+        uint256 mintAmount = (msg.value * exchangeRatio) / 1e18;
+        ankrBNB.mint(msg.sender, mintAmount);
+    }
+}
---
+++ test/mocks/MockERC20.sol
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract MockERC20 {
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+
+    constructor(string memory name_, string memory symbol_) {
+        name = name_;
+        symbol = symbol_;
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        uint256 allowed = allowance[from][msg.sender];
+        if (allowed != type(uint256).max) {
+            require(allowed >= amount, "ALLOWANCE");
+            allowance[from][msg.sender] = allowed - amount;
+        }
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(balanceOf[from] >= amount, "BALANCE");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount) internal {
+        totalSupply += amount;
+        balanceOf[to] += amount;
+        emit Transfer(address(0), to, amount);
+    }
+
+    function _burn(address from, uint256 amount) internal {
+        require(balanceOf[from] >= amount, "BALANCE");
+        balanceOf[from] -= amount;
+        totalSupply -= amount;
+        emit Transfer(from, address(0), amount);
+    }
+}
+
+contract MockWBNB is MockERC20 {
+    constructor() MockERC20("Wrapped BNB", "WBNB") {}
+
+    function deposit() external payable {
+        _mint(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amount) external {
+        _burn(msg.sender, amount);
+        (bool success,) = msg.sender.call{value: amount}("");
+        require(success, "BNB_SEND_FAIL");
+    }
+
+    receive() external payable {}
+}
